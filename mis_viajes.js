bb1d8548333c783671c17313e914f4ec9a2814e6
juancc-00 <juan.canada.carril@gmail.com>
2024-09-18 21:55:03 +0200

    Add files via upload

---
+++ mis_viajes.js
@@ -0,0 +1,17 @@
+let mis_viajes = [
+    {destino: "Gredos", año: "2022", miniatura: "files/gredos.jpg", participantes: "Lalo, Elena, Juan, Alberto", duracion: '1 semana', icono: 'iconos/icono_alberto.PNG', tipo: 'Monte'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Creta", año: "2022", miniatura: 'files/creta.jpg', participantes: 'Lalo, Elena, Juan, Alberto, Inés', duracion: '10 días', icono: 'iconos/icono_helen.PNG'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'},
+    {destino: "Padua", año: "2023", miniatura: "files/padua.jpg", participantes: "Juan, Pelayo", duracion: '3 días', icono: 'iconos/icono_juan.PNG', tipo: 'Escapada'}
+    
+];
